ba93d87a306b0d2e64802f03b3fbc0184f780349
Kandarp Khandwala <kandarpck@gmail.com>
2018-03-08 02:29:48 -0500

    Initialize the JWT Framework

--- apple/apple/settings.py
+++ apple/apple/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import datetime
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,3 +127,8 @@ REST_FRAMEWORK = {
     ),
 }
 
+JWT_AUTH = {
+    'JWT_AUTH_HEADER_PREFIX': 'Token',
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(minutes=30)
+
+}
