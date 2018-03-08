a6f567241784923fd924cc97f18ddbb516bdb3c2
Kandarp Khandwala <kandarpck@gmail.com>
2018-03-08 02:33:38 -0500

    Create the Token creation, refresh and verification endpoints

--- apple/apple/urls.py
+++ apple/apple/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+
+from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^auth/api/token/obtain/', obtain_jwt_token),
+    url(r'auth/api/token/refresh/', refresh_jwt_token),
+    url(r'auth/api/token/verify/', verify_jwt_token),
+
 ]
